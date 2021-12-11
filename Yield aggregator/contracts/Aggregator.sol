81fd1f1eb29af28b59173ca71ecae74541cb0f6d
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2021-12-11 22:03:23 +0300

    Update Aggregator.sol

--- Yield aggregator/contracts/Aggregator.sol
+++ Yield aggregator/contracts/Aggregator.sol
@@ -28,7 +28,7 @@ interface aDAI {
 
 // Interface for Aave's lending pool contract
 interface AaveLendingPool {
-    function deposit(address asset, uint256 amount, address amount, address onBehalfOf, uint16 referralCode) external;
+    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
 
     function withdraw(address asset, uint256 amount, address to) external;
 

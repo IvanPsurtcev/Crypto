b2afbd78e03799f650b7b6b9f7a57feb8fde33f1
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2022-01-01 12:38:02 +0300

    v2

--- all-on-chain-generated-nft/hardhat.config.js
+++ all-on-chain-generated-nft/hardhat.config.js
@@ -35,11 +35,23 @@ module.exports = {
       },
       saveDeployments: true
     },
+    polygon: {
+      url: "https://polygon-rpc.com/"
+        accounts: {
+          mnemonic: MNEMONIC
+        },
+        saveDeployments: true
+      }
+    }
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
   },
-  solidity: "0.8.0",
+  solidity: {
+    compilers: [
+      { version: "0.8.0" }, { version: "0.4.24" }, { version: "0.6.6" }, { version: "0.7.0" }
+    ]
+  },
   namedAccounts: {
     deployer: {
       default: 0

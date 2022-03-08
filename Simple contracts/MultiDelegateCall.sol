685c90ee32f2be4feeea679f43ad81b4874dacf5
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2022-03-08 17:36:22 +0300

    add contract TestMultiDelegatecall

--- Simple contracts/MultiDelegateCall.sol
+++ Simple contracts/MultiDelegateCall.sol
@@ -20,3 +20,22 @@ contract MultiDelegatecall {
         }
     }
 }
+
+contract TestMultiDelegatecall is MultiDelegatecall {
+    event Log(address caller, string func, uint i);
+
+    function func1(uint x, uint y) external {
+        emit Log(msg.sender, "func1", x + y);
+    }
+
+    function func2() external returns (uint) {
+        emit Log(msg.sender, "func2", 2);
+        return 111;
+    }
+
+    mapping(address => uint) public balanceOf;
+
+    function mint() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
+}
